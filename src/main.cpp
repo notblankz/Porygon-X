d2b41f57a65f4bd895da93bc4354c9d326386d31
amankhandelwaal <imakhandelwal2005@gmail.com>
2025-03-25 09:28:22 +0530

    WiFi setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include "BluetoothSerial.h"
+#include "WiFi.h"
+
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
 
 BluetoothSerial SerialBT;
 
@@ -7,6 +11,18 @@ void setup() {
     Serial.begin(115200);
     SerialBT.begin("Porygon-X");
     Serial.println("Bluetooth Started! Waiting for connection...");
+
+    // connecting to WiFi
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status()!= WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected to WiFi");
+    Serial.print("ESP32 IP Address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
