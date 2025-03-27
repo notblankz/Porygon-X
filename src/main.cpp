6ca20b0a414021eface89e22d2be90b6dbe9f936
notblankz <aahanrandomg@gmail.com>
2025-03-27 14:07:17 +0530

    Quality of Life Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
 #include <Arduino.h>
 #include "BluetoothSerial.h"
 #include "WiFi.h"
+#include "../lib/secrets.h"
 
-const char *ssid = "oneplus";
-const char *password = "hahahaha";
+const int LED_PIN = 2;
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
 
 BluetoothSerial SerialBT;
 
+void customBlink() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+}
+
 void setup()
 {
-    Serial.begin(115200);
-    SerialBT.begin("Porygon-X");
-    Serial.println("Bluetooth Started! Waiting for connection...");
 
-    // connecting to WiFi
+    pinMode(LED_PIN, OUTPUT);
+
+    Serial.begin(115200);
+
+    // <--- Logger for Getting all WiFi connections --->
+    // Serial.println("Scanning for available networks...");
+    // int numNetworks = WiFi.scanNetworks();
+
+    // if (numNetworks == 0) {
+    //     Serial.println("No networks found");
+    // } else {
+    //     Serial.println("Discovered Networks");
+    //     for (int i = 0; i < numNetworks; i++) {
+    //         Serial.printf("%d: %s (Signal Strength: %d dBm)\n", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i));
+    //     }
+    // }
+
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED)
@@ -22,19 +53,24 @@ void setup()
         Serial.print(".");
     }
 
-    Serial.println("Connected to WiFi");
-    Serial.print("ESP32 IP Address: ");
+    Serial.println("\nConnected to WiFi");
+    customBlink();
+    Serial.print("\nESP32 IP Address: ");
     Serial.println(WiFi.localIP());
+
+    Serial.println("Bluetooth Started! Waiting for connection...");
+    SerialBT.begin("Porygon-X");
 }
 
 void loop()
 {
     if (SerialBT.available())
-    { // Check if data is received
+    {
         String receivedData = SerialBT.readStringUntil('\n');
         receivedData.trim();
-
-        Serial.print("Received: ");
-        Serial.println(receivedData);
+        digitalWrite(LED_PIN, HIGH);
+        delay(500);
+        digitalWrite(LED_PIN, LOW);
+        delay(1000);
     }
 }
