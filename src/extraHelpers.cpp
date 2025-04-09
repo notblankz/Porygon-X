cbde4a7303bea6e68afa6936580b0e15308e199e
notblankz <aahanrandomg@gmail.com>
2025-04-09 23:47:22 +0530

    Update pre-calibration LED blink pattern

--- src/extraHelpers.cpp
+++ src/extraHelpers.cpp
@@ -19,12 +19,13 @@ void customBlink() {
 void mpuCalibratingBlink() {
     const int LED_PIN = 2;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 8; i++) {
         digitalWrite(LED_PIN, HIGH);
-        delay(100);
+        delay(250);
         digitalWrite(LED_PIN, LOW);
-        delay(100);
+        delay(250);
     }
-
-    delay(500);
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
 }
